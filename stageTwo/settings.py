f44a4543699f5c74c6cbe01c4b1b24fb4575ea4d
Abubakar Kabir Auta <abubakarkabirauta@gmail.com>
2023-09-13 04:36:35 +0100

    d

--- stageTwo/settings.py
+++ stageTwo/settings.py
@@ -76,18 +76,14 @@ WSGI_APPLICATION = 'stageTwo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'abububakar',
-        'USER': 'abububakar_user',
-        'PASSWORD': 'KLFOXOzt7jXIUNj0KM2M13puq7gZyONb',
-        'HOST': 'dpg-ck0ibn36fquc73e57kjg-a',  # Provided by Render
-        'PORT': '5432',  # PostgreSQL default port
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+
 
 
 
